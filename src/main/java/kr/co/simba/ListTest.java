494635a187acd8924858e4ff8c0bb1b1c183eff7
geek9 <withpd@gmail.com>
2021-06-10 13:32:45 +0900

    commit

--- src/main/java/kr/co/simba/ListTest.java
+++ src/main/java/kr/co/simba/ListTest.java
@@ -12,6 +12,7 @@ public class ListTest {
 	static Map<String, Integer> list3 = new HashMap<String, Integer>(); 
 
 	public static void main(String[] args) {
+		
 		list1[0] = 1;
 		list1[1] = 2;
 		list1[2] = 3;

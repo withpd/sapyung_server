cc1a918f8cdedb34de6feac5acdfa74298108dfd
geek9 <withpd@gmail.com>
2021-06-11 12:33:28 +0900

    DB

---
+++ src/main/java/kr/co/sapyung/vo/BookVo.java
@@ -0,0 +1,47 @@
+package kr.co.sapyung.vo;
+
+public class BookVo {
+	private String author;
+	private String title;
+	private String price;
+	private String date;
+	private String publisher;
+	private String isbn;
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public String getDate() {
+		return date;
+	}
+	public void setDate(String date) {
+		this.date = date;
+	}
+	public String getPublisher() {
+		return publisher;
+	}
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+}
---
+++ src/main/java/kr/co/sapyung/vo/UserVO.java
@@ -0,0 +1,40 @@
+package kr.co.sapyung.vo;
+
+public class UserVO {
+    private String mail;
+    private String location;
+    private String pw;
+    private String time;
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPw() {
+        return pw;
+    }
+
+    public void setPw(String pw) {
+        this.pw = pw;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+}

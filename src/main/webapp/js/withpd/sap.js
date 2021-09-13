32564ec3b9352bc049b5149f964ebb13c06a5d0a
withpd <withpd@gmail.com>
2021-09-13 16:50:03 +0900

    sdf

--- src/main/webapp/js/withpd/sap.js
+++ src/main/webapp/js/withpd/sap.js
@@ -1,6 +1,6 @@
 function request() {
 	var type = "sapyoung";
-	var LOCAL_URL = "http://ec2-18-118-166-134.us-east-2.compute.amazonaws.com:8080/sapyoung/sapyoung";
+	var LOCAL_URL = "ec2-18-118-166-134.us-east-2.compute.amazonaws.com:8080/sapyoung/sapyoung";
 	 $(document).ready(function(){
         })
         .ajaxStart(function(){
